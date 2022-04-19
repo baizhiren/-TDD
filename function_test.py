a29f81ea2c3cebbc1cf48d4ab9ba4349a9d1a84f
wnc <820420684@qq.com>
2022-04-19 13:52:49 +0800

    First unit test and url mapping, dummy view

--- function_test.py
+++ function_test.py
@@ -1,7 +1,45 @@
 from selenium import webdriver
+import unittest
 
-chrome_driver=r"C:\Program Files\Google\Chrome\Application\chromedriver.exe"
-brower = webdriver.Chrome(executable_path=chrome_driver)
-brower.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        chrome_driver = r"C:\Program Files\Google\Chrome\Application\chromedriver.exe"
+        self.browser =  webdriver.Chrome(executable_path = chrome_driver)
+    
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. 
+        # She goes to check out its homepage.
+        self.browser.get('http://localhost:8000')
 
-assert "The install worked successfully! Congratulations!" in brower.title
+        # she notice the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title), "Browser title was: " + self.browser.title
+        self.fail("Finish the test!")
+
+if __name__ == '__main__':
+    unittest.main(warnings = 'ignore')
+    
+
+
+#she types "Bye some peacock feathers" into a text box(Edith's hobby is trying fly-fishing lures) 
+
+#when she hits enter, the page updates, and now the page lists 
+#"1: Buy peacock feathers" as an item in a to-do list
+
+#There is still a text box inviting her to add another item. She
+#enters "Use peacock feathers to make a fly" (Edith us very methodical)
+
+#the page updates again, and now shows both items on her list
+
+#Edith wonder whether the site will remember her list. Then she sees
+#that the site has generated a unique URL for her -- there is some 
+#explanatory text to that effect
+
+#she visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep 
+
+
+
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,18 @@
+from urllib import request, response
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
 
-# Create your tests here
-class SmokeTest(TestCase):
-    def  test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+    def test_root_url_resolve_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.contene.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertTrue(html.endswith('</html>'))
