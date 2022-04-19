fec5c28011d71f7e4ad6a95bab355c833d44c926
wnc <820420684@qq.com>
2022-04-19 14:10:34 +0800

    Basic view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -13,6 +13,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        html = response.contene.decode('utf8')
+        html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
