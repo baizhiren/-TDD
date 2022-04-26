3d80113809d95794ac32cbbe6dc014847d06e036
wnc <820420684@qq.com>
2022-04-26 13:57:24 +0800

    restruct code 1

--- function_test.py
+++ function_test.py
@@ -1,4 +1,9 @@
+from email import header
+import imp
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,30 +21,50 @@ class NewVisitorTest(unittest.TestCase):
 
         # she notice the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title), "Browser title was: " + self.browser.title
+        
+        #header_text = self.browser.find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do',header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        #she types "Bye some peacock feathers" into a text box(Edith's hobby is trying fly-fishing lures) 
+        inputbox.send_keys('Buy peacock feathers')
+
+        #when she hits enter, the page updates, and now the page lists 
+        #"1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.Enter)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        #There is still a text box inviting her to add another item. She
+        #enters "Use peacock feathers to make a fly" (Edith us very methodical)
         self.fail("Finish the test!")
+
+        #the page updates again, and now shows both items on her list
+
+        #Edith wonder whether the site will remember her list. Then she sees
+        #that the site has generated a unique URL for her -- there is some 
+        #explanatory text to that effect
+
+        #she visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep 
+
 
 if __name__ == '__main__':
     unittest.main(warnings = 'ignore')
     
 
-
-#she types "Bye some peacock feathers" into a text box(Edith's hobby is trying fly-fishing lures) 
-
-#when she hits enter, the page updates, and now the page lists 
-#"1: Buy peacock feathers" as an item in a to-do list
-
-#There is still a text box inviting her to add another item. She
-#enters "Use peacock feathers to make a fly" (Edith us very methodical)
-
-#the page updates again, and now shows both items on her list
-
-#Edith wonder whether the site will remember her list. Then she sees
-#that the site has generated a unique URL for her -- there is some 
-#explanatory text to that effect
-
-#she visits that URL - her to-do list is still there.
-
-# Satisfied, she goes back to sleep 
 
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from urllib import request, response
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -11,9 +12,15 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        html = response.content.decode('utf8')
+        self.assertTemplateUsed(response, 'home.html')
+
         request = HttpRequest()
         response = home_page(request)
-        html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
+    
+
+        
