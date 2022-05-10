a6485ace420fdfa2f3eac74edc61c025596551c4
wnc <820420684@qq.com>
2022-05-10 16:57:39 +0800

    after refrector

--- function_test.py
+++ function_test.py
@@ -16,7 +16,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -56,9 +56,6 @@ class NewVisitorTest(unittest.TestCase):
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feather to make a fly')
-
-
-
 
 
 
