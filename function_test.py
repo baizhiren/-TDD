19d2ef5513c60921bd03ba63143ae04ed6d31478
wnc <820420684@qq.com>
2022-05-10 16:52:28 +0800

    before refrector

--- function_test.py
+++ function_test.py
@@ -13,7 +13,12 @@ class NewVisitorTest(unittest.TestCase):
     
     def tearDown(self):
         self.browser.quit()
-    
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. 
         # She goes to check out its homepage.
@@ -40,6 +45,7 @@ class NewVisitorTest(unittest.TestCase):
         #"1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feather')
 
         #There is still a text box inviting her to add another item. She
         #enters "Use peacock feathers to make a fly" (Edith us very methodical)
@@ -48,16 +54,18 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feather to make a fly')
 
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+
+
+
+
         # self.assertTrue(
         #     any(row.text == '1: Buy peacock feathers' for row in rows),
         #     f"New to do item did not appear in table. Contents were:\n{table.text}"
         # )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feather to make a fly', [row.text for row in rows])
 
 
        
