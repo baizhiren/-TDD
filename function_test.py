74d4b2adbb6aef92455a7b3b61471d52bb71782c
wnc <820420684@qq.com>
2022-05-10 16:44:41 +0800

    before refrector

--- function_test.py
+++ function_test.py
@@ -40,17 +40,27 @@ class NewVisitorTest(unittest.TestCase):
         #"1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-       
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to do item did not appear in table"
-        )
 
         #There is still a text box inviting her to add another item. She
         #enters "Use peacock feathers to make a fly" (Edith us very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feather to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     f"New to do item did not appear in table. Contents were:\n{table.text}"
+        # )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feather to make a fly', [row.text for row in rows])
+
+
+       
         self.fail("Finish the test!")
 
         #the page updates again, and now shows both items on her list
--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,6 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        return HTTPResponse(request.POST['item_text'])
-    return render(request, 'home.html') 
+    return render(request, 'home.html', {'new_item_text' : request.POST.get('item_text','') }) 
