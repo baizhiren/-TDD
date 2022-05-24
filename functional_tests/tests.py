c2237368d41245aa8d344a190d73a658c259d81f
wnc <820420684@qq.com>
2022-05-24 13:57:17 +0800

    Add a new user in system

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,26 +1,82 @@
 from email import header
+import imp
+from re import M
 from urllib import response
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 import unittest
-
+MAX_WAIT = 10
+chrome_driver = r"C:\Program Files\Google\Chrome\Application\chromedriver.exe"
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
-        chrome_driver = r"C:\Program Files\Google\Chrome\Application\chromedriver.exe"
         self.browser =  webdriver.Chrome(executable_path = chrome_driver)
     
     def tearDown(self):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return 
+            except (AssertionErrorm, WebDriverException) as e:
+                if time.time() - start_time < MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    
+    def test_multiple_users_can_start_lists_at_differents_urls(self):
+        #Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+        #She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        #Now use a new browser session to make sure that no imformation
+        ## of Edith's  is comming through  from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Chrome(executable_path = chrome_driver)
+
+        #Francis visits the home page. There is no sign of Edith's
+        #list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        #Francis start a new list by entering a new. He
+        #is less interesting than Edit
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy milk')
+
+        #Francis gets his own uqniu URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        #Again there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        #satisfy
+
+            
+
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. 
         # She goes to check out its homepage.
         
@@ -47,7 +103,6 @@ class NewVisitorTest(LiveServerTestCase):
         #when she hits enter, the page updates, and now the page lists 
         #"1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         #There is still a text box inviting her to add another item. She
@@ -67,8 +122,7 @@ class NewVisitorTest(LiveServerTestCase):
         # )
 
 
-       
-        self.fail("Finish the test!")
+    
 
         #the page updates again, and now shows both items on her list
 
