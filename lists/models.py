d267f9816fc3da45550fbec8a8e6d665c49019c0
wnc <820420684@qq.com>
2022-05-31 18:43:10 +0800

    after refreator

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,10 @@
+from statistics import mode
 from django.db import models
 # Create your models here.
 
+class List(models.Model):
+    pass
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)   
--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 from lists.views import home_page
 
@@ -22,15 +22,24 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
     
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retriving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
+
 
         second_item = Item()
         second_item.text= 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -39,16 +48,18 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item. text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
         response = self.client.get('/lists/the-only-list-in-the-world/')
-
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
 
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
@@ -65,12 +76,3 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
-
-
-
-
-        
-
-
-
-        
