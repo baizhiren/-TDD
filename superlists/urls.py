96f06a382ae19a13325800b2a526289e5d2ced55
wnc <820420684@qq.com>
2022-05-31 13:41:23 +0800

    add url and new view

--- superlists/urls.py
+++ superlists/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     #url(r'^lists/the-only-world-in-the-world/$', view.view_list, name='view_list'),
     path('lists/the-only-list-in-the-world/', views.view_list, name = 'view_list'),
     path('', views.home_page, name = 'home'),
+    path('lists/new', views.new_list, name='new_list')
 ]
