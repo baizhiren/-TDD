7c06fdb79147dc2424c9bce98ece1937b90b05b8
wnc <820420684@qq.com>
2022-06-04 22:32:50 +0800

    first steps of FT for layout + styling

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from lists import views
-
+from lists import urls as list_urls
 
 urlpatterns = [
     path('', views.home_page, name = 'home'),
-    path('lists/new', views.new_list, name='new_list'),
-    re_path(r'^lists/(\d+)/$', views.view_list, name = 'view_list'),
-    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item')
+    # path('lists/new', views.new_list, name='new_list'),
+    # re_path(r'^lists/(\d+)/$', views.view_list, name = 'view_list'),
+    # re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item')
+    re_path(r'^lists/', include(list_urls)),
 ]
